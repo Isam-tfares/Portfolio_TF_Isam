231f04eec14249edb16d35abdbf55f263bf4c4cb
Isam-tfares <tfaresisam@gmail.com>
2023-04-04 11:09:52 +0000

    Updated app

--- src/Components/Skill.js
+++ src/Components/Skill.js
@@ -1,9 +1,9 @@
-export default function Skill(props){
-    return(<div className="m-auto my-3 Skill    col-lg-2 col-md-3 col-sm-4 col-6  justify-content-center d-flex align-items-center">
+export default function Skill(props) {
+    return (<div className="m-auto my-3 Skill    col-lg-2 col-md-3 col-sm-4 col-6  justify-content-center d-flex align-items-center">
         <div className=" text-center ">
-            <img className="img-fluid" src={props.src} alt={props.name}></img>
+            <img className="img-fluid" src={props.src} alt={props.name} style={{ width: '48px', height: '48px' }}></img>
             <p className="text-secondary fw-light">{props.name}</p>
         </div>
-        
+
     </div>)
 }
--- src/data/DataSkills.js
+++ src/data/DataSkills.js
@@ -1,21 +1,44 @@
-const DataSkills=[
+const DataSkills = [
     {
         "name": "Python",
         "icon": "https://img.icons8.com/color/48/000000/python--v1.png"
     },
-   
+
     {
         "name": "ReactJS",
         "icon": "https://img.icons8.com/external-tal-revivo-color-tal-revivo/48/000000/external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo.png"
+    }, {
+        "name": "React native",
+        "icon": "https://img.icons8.com/nolan/64/react-native.png"
+    },{
+        'name':'Kotlin',
+        'icon':"https://img.icons8.com/color/48/null/kotlin.png"
+    },
+    {
+        'name': 'Expo Go',
+        'icon': 'https://camo.githubusercontent.com/ac51c2a5c1e1b564cce09fd58c6005a9360230a8d60486eb48ab029bbb12a752/68747470733a2f2f617070726563732e6f72672f67702f696d616765732f6170702d69636f6e732f3330302f64382f686f73742e6578702e6578706f6e656e742e6a7067'
+    },
+    {
+        'name': 'Tailwind',
+        'icon': "https://img.icons8.com/color/48/null/tailwindcss.png"
+    },
+    {
+        'name': 'Sass',
+        'icon': "https://img.icons8.com/color/48/null/sass.png"
+    },
+    {
+        "name": "Bootstrap",
+        "icon": "https://img.icons8.com/color/48/000000/bootstrap.png"
     },
     {
         "name": "NodeJS",
         "icon": "https://img.icons8.com/color/48/000000/nodejs.png"
     },
     {
-        "name": "Bootstrap",
-        "icon": "https://img.icons8.com/color/48/000000/bootstrap.png"
+        "name": "Sqlite",
+        "icon": "https://img.icons8.com/office/16/null/data-configuration.png"
     },
+
     {
         "name": "HTML5",
         "icon": "https://img.icons8.com/color/48/000000/html-5--v1.png"
@@ -26,13 +49,13 @@ const DataSkills=[
     },
     {
         "name": "C",
-        "icon": "https://img.icons8.com/color/48/null/c-plus-plus-logo.png"
+        "icon": "https://img.icons8.com/fluency/48/null/c-programming.png"
     },
     {
         "name": "Jquery",
         "icon": "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/null/external-jquery-is-a-javascript-library-designed-to-simplify-html-logo-shadow-tal-revivo.png"
     },
-    
+
     {
         "name": "JavaScript",
         "icon": "https://img.icons8.com/color/48/000000/javascript--v1.png"
@@ -68,7 +91,7 @@ const DataSkills=[
     {
         "name": "WordPress",
         "icon": "https://img.icons8.com/color/48/000000/wordpress.png"
-    },   
+    },
     {
         "name": "Figma",
         "icon": "https://img.icons8.com/color/48/figma--v1.png"
